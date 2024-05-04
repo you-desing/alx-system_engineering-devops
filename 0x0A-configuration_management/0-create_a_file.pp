bccf99b01c7cd6e8d91b887b0c8c60f817d72dba
you-desing <youssefboulahya3@gmail.com>
2024-05-04 09:35:49 -0700

    config

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file using puppet
+
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
